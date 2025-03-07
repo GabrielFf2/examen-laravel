0d7aa94a169b66a5d9c625d8c7b0482919acefbe
GabrielFf2 <149058439+GabrielFf2@users.noreply.github.com>
2025-03-07 10:11:08 +0100

    tots els endpoints funcionals

--- examen/app/Http/Controllers/LlibreAutorController.php
+++ examen/app/Http/Controllers/LlibreAutorController.php
@@ -1,7 +1,55 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\authors;
+use App\Models\llibres;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 class LlibreAutorController
 {
 
+    public function assign($idLlibre , $idAutor)
+    {
+        $llibresAuthors = DB::table('author_llibre')->insert([
+            'llibre_id' => $idLlibre,
+            'author_id' => $idAutor
+        ]);
+        return response()->json($llibresAuthors);
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function unassign($idLlibre , $idAutor)
+    {
+        $llibresAuthors = DB::table('author_llibre')->where(
+            'llibre_id', $idLlibre
+        )->where('author_id', $idAutor)->delete();
+        return response()->json($llibresAuthors);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function getAutors($idLlibre )
+    {
+        $llibresAuthors = DB::table('author_llibre')->where('llibre_id', $idLlibre)->get();
+        $authors = authors::all()->whereIn('id', $llibresAuthors->pluck('author_id'));
+        return response()->json($authors);
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function getLlibres($idAuthor)
+    {
+
+        $llibresAuthors = DB::table('author_llibre')->where('author_id', $idAuthor)->get();
+        $llibres = llibres::all()->where('id', $llibresAuthors->pluck('llibre_id'));
+        return response()->json($llibres);
+
+    }
 }
